d6185ecb216ffc5bf1bff8afa38143ee852650e1
David Kendall <david.kendall@northumbria.ac.uk>
2019-07-28 10:23:42 +0100

    Add accounts for gmail and hesabu

--- offlineimap/offlineimap.py
+++ offlineimap/offlineimap.py
@@ -1,5 +1,12 @@
 #! /usr/bin/env python2
 from subprocess import check_output
 
-def get_pass():
-    return check_output("gpg -dq --default-key F8C034F1 ~/.mutt_offline/northumbria_pass.gpg", shell=True).strip("\n")
+def get_pass(acc):
+    if acc == 'northumbria':
+        return check_output("gpg -dq --default-key F8C034F1 ~/.mutt_offline/northumbria_pass.gpg", shell=True).strip("\n")
+    elif acc == 'hesabu':
+        return check_output("gpg -dq --default-key F8C034F1 ~/.mutt_offline/hesabu_pass.gpg", shell=True).strip("\n")
+    else:
+        return check_output("gpg -dq --default-key F8C034F1 ~/.mutt_offline/gmail_pass.gpg", shell=True).strip("\n")
+    
+
